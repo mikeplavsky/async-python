c084fb6939d7e22e33d7629cec9202b0935fb9b4
mikeplavsky <mike.plavsky@quest.com>
2015-10-02 12:02:57 +0300

    implemented golang

--- go-pt/main.go
+++ go-pt/main.go
@@ -10,11 +10,9 @@ import (
 
 var pt_url = "https://www.pivotaltracker.com/services/v5/projects"
 
-func main() {
+func get(url string, token string) []map[string]interface{} {
 
-	token := os.Args[1]
-
-	req, _ := http.NewRequest("GET", pt_url, nil)
+	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("X-TrackerToken", token)
 
 	c := http.Client{}
@@ -25,13 +23,90 @@ func main() {
 	data := []map[string]interface{}{}
 	json.Unmarshal(body, &data)
 
+	return data
+
+}
+
+type info struct {
+	name string
+	num  int
+}
+
+type progress struct {
+	id  int
+	num int
+}
+
+func get_releases(name string,
+	proj_id int,
+	token string,
+	res chan<- info,
+	p chan<- progress) {
+
+	offset := 0
+
+	for {
+
+		url := fmt.Sprintf(
+
+			"%v/%v/iterations?scope=current_backlog&offset=%v",
+			pt_url,
+			proj_id,
+			offset)
+
+		data := get(url, token)
+		p <- progress{proj_id, offset}
+
+		if len(data) == 0 {
+			break
+		}
+
+		offset += len(data)
+
+	}
+
+	res <- info{name, offset}
+
+}
+
+func get_progress(pr <-chan progress) {
+
+	for {
+		fmt.Println(<-pr)
+	}
+}
+
+func main() {
+
+	token := os.Args[1]
+	data := get(pt_url, token)
+
+	r := make(chan info)
+	pr := make(chan progress)
+
+	go get_progress(pr)
+
 	for _, p := range data {
 
 		id := int(p["id"].(float64))
+		name := p["name"].(string)
 
 		fmt.Println(
 			id,
-			p["name"])
+			name)
+
+		go get_releases(
+			name, id, token, r, pr)
 	}
+
+        i := []info {}
+
+	for _, _ = range data {
+		i = append(i, <-r)
+	}
+
+        for _, p := range i {
+            fmt.Println(p)
+        }
 
 }
